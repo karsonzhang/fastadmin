2a9081c7d9463b9e0d5d557cea6087cfcbacdfa4
Karson <karsonzhang@163.com>
2017-04-23 12:25:41 +0800

    移除后台无关的JS文件 修复上传链接错误

--- public/assets/js/backend/general/attachment.js
+++ public/assets/js/backend/general/attachment.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'form', 'table'], function ($, undefined, Backend, Form, Table) {
+define(['jquery', 'bootstrap', 'backend', 'form', 'table', 'config'], function ($, undefined, Backend, Form, Table, Config) {
 
     var Controller = {
         index: function () {
@@ -24,14 +24,14 @@ define(['jquery', 'bootstrap', 'backend', 'form', 'table'], function ($, undefin
                     [
                         {field: 'state', checkbox: true, },
                         {field: 'id', title: __('Id')},
-                        {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
+                        {field: 'url', title: __('Preview'), formatter: Controller.api.formatter.thumb},
+                        {field: 'url', title: __('Url'), formatter: Controller.api.formatter.url},
                         {field: 'imagewidth', title: __('Imagewidth')},
                         {field: 'imageheight', title: __('Imageheight')},
                         {field: 'imagetype', title: __('Imagetype')},
                         {field: 'imageframes', title: __('Imageframes')},
                         {field: 'filesize', title: __('Filesize')},
                         {field: 'mimetype', title: __('Mimetype')},
-                        {field: 'extparam', title: __('Extparam')},
                         {field: 'createtime', title: __('Createtime'), formatter: Table.api.formatter.datetime},
                         {field: 'operate', title: __('Operate'), events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
@@ -51,6 +51,19 @@ define(['jquery', 'bootstrap', 'backend', 'form', 'table'], function ($, undefin
         api: {
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
+            },
+            formatter: {
+                thumb: function (value, row, index) {
+                    console.log(row);
+                    if (row.mimetype.indexOf("image") > -1) {
+                        return '<a href="' + Config.upload.cdnurl + value + '" target="_blank"><img src="' + Config.upload.cdnurl + value + '!/fwfh/50x50" alt=""></a>';
+                    } else {
+                        return '无';
+                    }
+                },
+                url: function (value, row, index) {
+                    return '<a href="' + Config.upload.cdnurl + value + '" target="_blank" class="label bg-green">' + value + '</a>';
+                },
             }
         }
 
