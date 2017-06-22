c122557d8a3bd32eebec0333c5432c1bd90faa42
F4NNIU <sparkamax@gmail.com>
2017-06-22 17:29:02 +0800

    修改数据库配置的密码.

--- application/database.php
+++ application/database.php
@@ -20,7 +20,7 @@ return [
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
