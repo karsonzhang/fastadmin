4893d5fe75506f029f47f186e6414d7a99dae764
Karson <karsonzhang@163.com>
2018-01-20 17:16:54 +0800

    新增fa_sms表结构

--- application/admin/command/Install/fastadmin.sql
+++ application/admin/command/Install/fastadmin.sql
@@ -338,6 +338,21 @@ INSERT INTO `fa_config` VALUES (17, 'mail_from', 'email', 'Mail from', '', 'stri
 COMMIT;
 
 -- ----------------------------
+-- Table structure for fa_sms
+-- ----------------------------
+DROP TABLE IF EXISTS `fa_sms`;
+CREATE TABLE `fa_sms` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `event` varchar(30) NOT NULL DEFAULT '' COMMENT '事件',
+  `mobile` varchar(20) NOT NULL DEFAULT '' COMMENT '手机号',
+  `code` varchar(10) NOT NULL DEFAULT '' COMMENT '验证码',
+  `times` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '验证次数',
+  `ip` varchar(30) NOT NULL DEFAULT '' COMMENT 'IP',
+  `createtime` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='短信验证码表';
+
+-- ----------------------------
 -- Table structure for fa_test
 -- ----------------------------
 DROP TABLE IF EXISTS `fa_test`;
