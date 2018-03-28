ba5f8675aa008f137f86a4969a824a2601dc92f4
Strangere <stranger1688@vip.qq.comm>
2018-03-28 18:59:32 +0800

    修改验证码右边字节的一半

--- application/config.php
+++ application/config.php
@@ -228,15 +228,15 @@ return [
         // 验证码字符集合
         'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
         // 验证码字体大小(px)
-        'fontSize' => 16,
+        'fontSize' => 18,
         // 是否画混淆曲线
         'useCurve' => false,
         //使用中文验证码
         'useZh'    => false,
         // 验证码图片高度
-        'imageH'   => 30,
+        'imageH'   => 40,
         // 验证码图片宽度
-        'imageW'   => 100,
+        'imageW'   => 130,
         // 验证码位数
         'length'   => 4,
         // 验证成功后是否重置
