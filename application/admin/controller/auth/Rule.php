02029bf972d91e856745afd9c9fe3ab57bde40b4
pppscn <35696959@qq.com>
2019-02-16 14:41:53 +0800

    优化规则ruleList排序

--- application/admin/controller/auth/Rule.php
+++ application/admin/controller/auth/Rule.php
@@ -27,7 +27,7 @@ class Rule extends Backend
         parent::_initialize();
         $this->model = model('AuthRule');
         // 必须将结果集转换为数组
-        $ruleList = collection($this->model->order('weigh', 'desc')->select())->toArray();
+        $ruleList = collection($this->model->order('weigh', 'desc')->order('id', 'asc')->select())->toArray();
         foreach ($ruleList as $k => &$v)
         {
             $v['title'] = __($v['title']);
@@ -43,6 +43,7 @@ class Rule extends Backend
                 continue;
             $ruledata[$v['id']] = $v['title'];
         }
+        unset($v);
         $this->view->assign('ruledata', $ruledata);
     }
 
