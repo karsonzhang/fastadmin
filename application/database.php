7f07bdb0f424cfb999922fe7df3866b0d079da17
chaofu <chaofu@163.com>
2019-05-15 10:47:10 +0800

    数据库修改

--- application/database.php
+++ application/database.php
@@ -22,7 +22,7 @@ return [
     // 用户名
     'username'        => Env::get('database.username', 'root'),
     // 密码
-    'password'        => Env::get('database.password', 'Kuai!Le@221'),
+    'password'        => Env::get('database.password', 'KuaiLe@221'),
     // 端口
     'hostport'        => Env::get('database.hostport', ''),
     // 连接dsn
