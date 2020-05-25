8fb48352ed692a215c83a3c4617e23627e9a4d2b
萤火虫 <1311915984@qq.com>
2020-05-25 16:00:02 +0800

    修复Bug: 关闭Debug的情况下,切换语言时,请求语言包的url没有变化,使得浏览器使用了缓存,导致没能成功切换语言

--- public/assets/js/require-backend.js
+++ public/assets/js/require-backend.js
@@ -142,7 +142,7 @@ require(['jquery', 'bootstrap'], function ($, undefined) {
     window.Config = Config;
     // 配置语言包的路径
     var paths = {};
-    paths['lang'] = Config.moduleurl + '/ajax/lang?callback=define&controllername=' + Config.controllername;
+    paths['lang'] = Config.moduleurl + '/ajax/lang?callback=define&controllername=' + Config.controllername + '&lang=' + Config.language;
     // 避免目录冲突
     paths['backend/'] = 'backend/';
     require.config({paths: paths});
