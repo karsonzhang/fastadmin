cdd53ac377a3a6e4058b0f1f39f4eeec48d74353
Karson <karsonzhang@163.com>
2020-09-04 14:22:13 +0800

    优化安装脚本检测

--- public/admin.php
+++ public/admin.php
@@ -17,8 +17,7 @@ define('APP_PATH', __DIR__ . '/../application/');
 
 // 判断是否安装
 if (!is_file(APP_PATH . 'admin/command/Install/install.lock')) {
-    $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-    header("Location: {$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}{$uri}/install.php");
+    header("location:./install.php");
     exit;
 }
 
