dfeff713bd840f135af72148e3a4bf44f6dd0ed2
Karson <karsonzhang@163.com>
2022-05-27 15:46:18 +0800

    优化配置打包JS

--- application/config.php
+++ application/config.php
@@ -266,7 +266,7 @@ return [
         //会员注册验证码类型email/mobile/wechat/text/false
         'user_register_captcha' => 'text',
         //登录验证码
-        'login_captcha'         => false,
+        'login_captcha'         => true,
         //登录失败超过10次则1天后重试
         'login_failure_retry'   => true,
         //是否同一账号同一时间只能在一个地方登录
