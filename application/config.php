676536878151073f46d2322da7a9942102d97257
Karson <karsonzhang@163.com>
2023-03-15 16:39:10 +0800

    修改版本号

--- application/config.php
+++ application/config.php
@@ -303,7 +303,7 @@ return [
         //允许跨域的域名,多个以,分隔
         'cors_request_domain'   => 'localhost,127.0.0.1',
         //版本号
-        'version'               => '1.3.5.20221214',
+        'version'               => '1.4.0.20230315',
         //API接口地址
         'api_url'               => 'https://api.fastadmin.net',
     ],
