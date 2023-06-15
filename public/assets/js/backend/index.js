0e75d1d6cdd820220d92219311e3abb25bf44363
Karson <karsonzhang@163.com>
2023-06-15 21:32:42 +0800

    优化插件管理

--- public/assets/js/backend/index.js
+++ public/assets/js/backend/index.js
@@ -204,7 +204,7 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'adminlte', 'form'], functi
             }
 
             //这一行需要放在点击左侧链接事件之前
-            var addtabs = Config.referer ? localStorage.getItem("addtabs") : null;
+            var addtabs = Config.referer ? sessionStorage.getItem("addtabs") : null;
 
             console.log($("[data-config='simplenav']").prop("checked"))
             //绑定tabs事件,如果需要点击强制刷新iframe,则请将iframeForceRefresh置为true,iframeForceRefreshTable只强制刷新表格
