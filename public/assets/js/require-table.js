bfd3737b286ee6796317b0489e53cc6e2997b237
Karson <karsonzhang@163.com>
2023-06-15 14:36:08 +0800

    按钮配置属性extend支持function

--- public/assets/js/require-table.js
+++ public/assets/js/require-table.js
@@ -882,7 +882,7 @@ define(['jquery', 'bootstrap', 'moment', 'moment/locale/zh-cn', 'bootstrap-table
                         refresh = j.refresh ? 'data-refresh="' + j.refresh + '"' : '';
                         confirm = typeof j.confirm === 'function' ? j.confirm.call(table, row, j) : (typeof j.confirm !== 'undefined' ? j.confirm : false);
                         confirm = confirm ? 'data-confirm="' + confirm + '"' : '';
-                        extend = j.extend ? j.extend : '';
+                        extend = typeof j.extend === 'function' ? j.extend.call(table, row, j) : (typeof j.extend !== 'undefined' ? j.extend : '');
                         disable = typeof j.disable === 'function' ? j.disable.call(table, row, j) : (typeof j.disable !== 'undefined' ? j.disable : false);
                         if (disable) {
                             classname = classname + ' disabled';
