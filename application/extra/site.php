f932156c8e71087a3439e34c909e0ae73634e09f
Karson <karsonzhang@163.com>
2024-03-28 11:55:34 +0800

    修改默认配置

--- application/extra/site.php
+++ application/extra/site.php
@@ -33,8 +33,8 @@ return [
     'mail_type' => '1',
     'mail_smtp_host' => 'smtp.qq.com',
     'mail_smtp_port' => '465',
-    'mail_smtp_user' => '10000',
-    'mail_smtp_pass' => 'password',
+    'mail_smtp_user' => '',
+    'mail_smtp_pass' => '',
     'mail_verify_type' => '2',
-    'mail_from' => '10000@qq.com',
+    'mail_from' => '',
 ];
